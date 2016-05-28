7689310df2859d453ffc6c08fda814659185f1a3
Daniel Patterson <dan24patt@gmail.com>
2016-05-28 11:35:21 +0100

    Add a single route that combines all sources for a location

--- compiled/routes.js
+++ compiled/routes.js
@@ -1,21 +1,37 @@
 var express_1 = require("express");
 var R = require("ramda");
-var Location = require("./locations");
+var Promise = require("bluebird");
+var Locations = require("./locations");
 var index_1 = require("./sources/index");
 var indexedSources = R.indexBy(R.prop("id"), index_1.Sources);
 var router = express_1.Router();
 router.get("/api/locations", function (request, response) {
     var term = request.query.like;
     var limit = request.query.limit;
-    Location.getLocationsLike(term, limit).then(response.send.bind(response));
+    Locations.getLocationsLike(term, limit).then(response.send.bind(response));
 });
 router.get("/api/sources", function (request, response) {
     response.send(index_1.Sources);
 });
+router.get("/api/forecasts/:locationId", function (request, response) {
+    Locations.findById(request.params.locationId)
+        .then(fetchFromAllSources)
+        .then(response.send.bind(response));
+});
 router.get("/api/forecasts/:locationId/sources/:sourceId", function (request, response) {
     var source = indexedSources[request.params.sourceId];
-    Location.findById(request.params.locationId)
+    Locations.findById(request.params.locationId)
         .then(source.fetchForecast)
         .then(response.send.bind(response));
 });
 exports.default = router;
+function fetchFromAllSources(location) {
+    return Promise
+        .all(index_1.Sources.map(fetchForSource(location)))
+        .then(function (sources) { return ({ sources: sources }); });
+}
+var fetchForSource = R.curry(function (location, source) {
+    return source
+        .fetchForecast(location)
+        .then(function (forecast) { return ({ source: source, forecast: forecast }); });
+});
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import * as R from "ramda";
+import * as Promise from "bluebird";
 
-import * as Location from "./locations";
+import * as Locations from "./locations";
 import { Sources } from "./sources/index";
+import { Source } from "./sources/source";
+import { ForecastPoint } from "./sources/forecast-point";
 
 const indexedSources = R.indexBy(R.prop("id"), Sources);
 
@@ -11,18 +14,45 @@ const router = Router();
 router.get("/api/locations", (request, response) => {
   const term: string  = request.query.like;
   const limit: number = request.query.limit;
-  Location.getLocationsLike(term, limit).then(response.send.bind(response));
+  Locations.getLocationsLike(term, limit).then(response.send.bind(response));
 });
 
 router.get("/api/sources", (request, response) => {
   response.send(Sources);
 });
 
+router.get("/api/forecasts/:locationId", (request, response) => {
+  Locations.findById(request.params.locationId)
+    .then(fetchFromAllSources)
+    .then(response.send.bind(response));
+});
+
 router.get("/api/forecasts/:locationId/sources/:sourceId", (request, response) => {
   const source = indexedSources[request.params.sourceId];
-  Location.findById(request.params.locationId)
+  Locations.findById(request.params.locationId)
     .then(source.fetchForecast)
     .then(response.send.bind(response));
 });
 
 export default router;
+
+interface MultiSourceResponse {
+  sources: SourceResponse[];
+}
+
+interface SourceResponse {
+  source: Source;
+  forecast: ForecastPoint[];
+}
+
+function fetchFromAllSources(location: Locations.Location): Promise<MultiSourceResponse> {
+  return Promise
+    .all(Sources.map(fetchForSource(location)))
+    .then(sources => ({ sources }));
+}
+
+const fetchForSource = R.curry((location: Locations.Location, source: Source): Promise<SourceResponse> => {
+  return source
+    .fetchForecast(location)
+    .then(forecast => ({ source, forecast }));
+  });
